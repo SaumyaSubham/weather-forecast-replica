f8ce44855e5c9c05de1c97b336efe83cd9572c13
Saumya Subham Mishra <109909356+SaumyaSubham@users.noreply.github.com>
2023-08-12 22:49:23 +0530

    Add files via upload

---
+++ cities.js
@@ -0,0 +1,39 @@
+export default [
+    {
+      country: "India",
+      name: "CUTTACK",
+      lat: "20.467",
+      lng: "85.933",
+    },
+    {
+      country: "India",
+      name: "BHUBANESHWAR",
+      lat: "20.244",
+      lng: "85.818",
+    },
+    {
+      country: "India",
+      name: "PURI",
+      lat: "29.14269",
+      lng: "71.25771",
+    },
+    {
+      country: "India",
+      name: "BALASORE",
+      lat: "21.517",
+      lng: "86.933",
+    },
+    {
+      country: "India",
+      name: "BARIPADA",
+      lat: "21.933",
+      lng: "86.767",
+    },
+    {
+      country: "India",
+      name: "CHENNAI",
+      lat: "12.994",
+      lng: "80.181",
+    },
+];
+  
